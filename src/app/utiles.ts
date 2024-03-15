10c6b774dd04128e16f3a613a9eda9363bbb5e6d
Saeed Hossen <appsaeed7@gmail.com>
2024-03-16 01:06:20 +0600

    firebase push notification

--- src/app/utiles.ts
+++ src/app/utiles.ts
@@ -7,6 +7,21 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 
+export function urlBase64ToUint8Array(base64String: string | any[]) {
+    const padding = "=".repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/-/g, "+")
+        .replace(/_/g, "/");
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
 
 //get todo from local storage 
 export function getTodoStore(): TodoType[] {
