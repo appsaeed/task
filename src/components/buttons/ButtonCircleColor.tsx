4a1f46f9b574463728379acdb257b3acbf3971f5
Saeed Hossen <appsaeed7@gmail.com>
2024-03-21 15:06:52 +0600

    change color buttons color

--- src/components/buttons/ButtonCircleColor.tsx
+++ src/components/buttons/ButtonCircleColor.tsx
@@ -6,12 +6,15 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export default function ButtonCircleColor({ color, checked, ...props }: Props) {
-    const colour = (color && color != "") ? color : 'neutral';
+    const colour: string = (color && color != "") ? color : 'neutral';
+    const colors: { [key: string]: string } = {
+        "red": cn("border-red-400 hover:bg-red-500", { "bg-red-600": checked }),
+        "green": cn("border-green-400 hover:bg-green-500", { "bg-green-600": checked }),
+        "yellow": cn("border-yellow-400 hover:bg-yellow-500", { "bg-yellow-600": checked }),
+        "neutral": cn("border-neutral-400 hover:bg-neutral-500", { "bg-neutral-600": checked }),
+    }
     return (
-        <button {...props} className={cn(`h-4 w-4 rounded-full border-2 cursor-pointer border-${colour}-400 hover:bg-${colour}-500`,
-            {
-                [`bg-${colour}-600`]: checked,
-            })} >
+        <button {...props} className={cn(`h-4 w-4 rounded-full border-2 cursor-pointer`, colors[colour])} >
 
         </button>
     )
